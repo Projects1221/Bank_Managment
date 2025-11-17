bad5ba373438ea318dbb80ddd2f1990a66681ed5
“[ram <[ramphapal03@gmail.com]>
2025-11-17 15:35:28 +0530

    first commit

---
+++ src/main/java/com/BankManagmentProjectApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankManagmentProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankManagmentProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/Repository/BankRepository.java
@@ -0,0 +1,8 @@
+package com.Repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository 
+public interface BankRepository {
+
+}
